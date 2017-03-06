4f3a372459bf15d8989a6d1ab33f6f94fb010589
Christoph Ruttkies <christoph.ruttkies@ipb-halle.de>
2017-03-06 17:12:52 +0100

    added scripts

---
+++ scripts/MS2ToMetFrag.R
@@ -0,0 +1,83 @@
+#!/usr/bin/env Rscript
+
+options(stringAsfactors = FALSE, useFancyQuotes = FALSE)
+
+# Taking the command line arguments
+args <- commandArgs(trailingOnly = TRUE)
+
+if(length(args)==0)stop("No file has been specified! Please select a file for performing RT correction!\n")
+
+inputMS2<-NA
+inputCamera<-NA
+output<-NA
+precursortppm<-10
+fragmentppm<-10
+fragmentabs<-0.001
+database<-"KEGG"
+for(arg in args)
+{
+  argCase<-strsplit(x = arg,split = "=")[[1]][1]
+  value<-strsplit(x = arg,split = "=")[[1]][2]
+  
+  if(argCase=="inputMS2")
+  {
+    inputMS2=as.character(value)
+  
+  }
+  if(argCase=="inputCAMERA")
+  {
+    
+    inputCamera=as.character(value)
+    
+  }
+  if(argCase=="precursorppm")
+  {
+    
+    precursortppm=as.numeric(value)
+    
+  }
+  if(argCase=="fragmentppm")
+  {
+    
+    fragmentppm=as.numeric(value)
+    
+  }
+  
+  if(argCase=="fragmentabs")
+  {
+    
+    fragmentabs=as.numeric(value)
+    
+  }
+  if(argCase=="database")
+  {
+    
+    database=as.character(value)
+    
+  }
+  if(argCase=="output")
+  {
+    output=as.character(value)
+  }
+}
+
+if(is.na(inputMS2) | is.na(inputCamera) | is.na(output)) stop("Both input (CAMERA and MS2 ) and output need to be specified!\n")
+
+settingsObject<-list()
+settingsObject[["DatabaseSearchRelativeMassDeviation"]]<-precursortppm
+settingsObject[["FragmentPeakMatchAbsoluteMassDeviation"]]<-fragmentabs
+settingsObject[["FragmentPeakMatchRelativeMassDeviation"]]<-fragmentppm
+settingsObject[["MetFragDatabaseType"]]<-database
+settingsObject[["MetFragScoreTypes"]]<-"FragmenterScore"
+load(inputMS2)
+MappedMS2s<-get(varNameForNextStep)
+load(inputCamera)
+cameraObject<-get(varNameForNextStep)
+
+
+source("adductCalculator.R")
+source("toMetfragCommand.R")
+library(stringr)
+toMetfragCommand(mappedMS2 = MappedMS2s$mapped,unmappedMS2 = MappedMS2s$unmapped,
+                 cameraObject = cameraObject,searchMultipleChargeAdducts = T,includeUnmapped = T,
+                 includeMapped = T,settingsObject = settingsObject,preprocess = F,savePath=output)
---
+++ scripts/readMS2MSnBase.R
@@ -0,0 +1,30 @@
+#!/usr/bin/env Rscript
+options(stringAsfactors = FALSE, useFancyQuotes = FALSE)
+
+# Taking the command line arguments
+args <- commandArgs(trailingOnly = TRUE)
+
+if(length(args)==0)stop("No file has been specified! Please select a file for performing peak picking!\n")
+require(MSnbase)
+RawFiles<-NA
+output<-NA
+for(arg in args)
+{
+  argCase<-strsplit(x = arg,split = "=")[[1]][1]
+  value<-strsplit(x = arg,split = "=")[[1]][2]
+  if(argCase=="input")
+  {
+    RawFiles=as.character(value)
+  }
+  if(argCase=="output")
+  {
+    output=as.character(value)
+  }
+  
+}
+if(is.na(RawFiles) | is.na(output)) stop("Both input and output need to be specified!\n")
+MS2RawFile<-readMSData(RawFiles, msLevel = 2, verbose = FALSE)
+
+preprocessingStepsMS2<-c("MS2RawFile")
+varNameForNextStep<-as.character("MS2RawFile")
+save(list = c("MS2RawFile","preprocessingStepsMS2","varNameForNextStep"),file = output)
